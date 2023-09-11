7722efa479699906aed23d30b712c984f55af5b1
Torpeedofv <torpeedo.fv@gmail.com>
2023-09-11 20:33:50 +0100

    task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for a function"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function that rotates a 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
