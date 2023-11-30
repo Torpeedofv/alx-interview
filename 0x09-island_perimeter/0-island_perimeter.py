656aa9e644bc3cd6b22a042e7af35ff7054ff54b
Torpeedofv <torpeedo.fv@gmail.com>
2023-11-30 12:51:19 +0100

    task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module for a function"""
+
+
+def island_perimeter(grid):
+    if not grid or len(grid) == 0:
+        return 0
+    rows = len(grid)
+    columns = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == 1:
+                perimeter += 4
+                if column > 0 and grid[row][column - 1] == 1:
+                    perimeter -= 2
+                if row > 0 and grid[row - 1][column] == 1:
+                    perimeter -= 2
+    return perimeter
